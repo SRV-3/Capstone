aa22198f70de0eeaa2950e6457a131ea1b67076d
Sourav <thakursourav778@gmail.com>
2026-05-17 14:25:17 +0530

    day2

--- sandbox/server/src/app.js
+++ sandbox/server/src/app.js
@@ -1,5 +1,8 @@
 import express from "express";
 import morgan from "morgan";
+import { createPod } from "../src/kubernetes/pod.js";
+import { createService } from "../src/kubernetes/service.js";
+import { v7 as uuid } from "uuid";
 
 const app = express();
 
@@ -11,6 +14,18 @@ app.get("/api/sandbox/health", (req, res) => {
   res.status(200).json({
     message: "Sandbox API is healthy",
     status: "ok",
+  });
+});
+
+app.post("/api/sandbox/start", async (req, res) => {
+  const sandboxId = uuid();
+
+  await Promise.all([createPod(sandboxId), createService(sandboxId)]);
+
+  return res.status(201).json({
+    message: "Sandbox environment created successfully",
+    sandboxId,
+    previewUrl: `http://${sandboxId}.preview.localhost`,
   });
 });
 
